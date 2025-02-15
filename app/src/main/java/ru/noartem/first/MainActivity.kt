678f0b75d040e1bfb82559075e959313fc16819b
Noskov Artem <nowasmawesome@gmail.com>
2025-02-15 15:28:42 +0500

    feat: increase greeting paddings

--- app/src/main/java/ru/noartem/first/MainActivity.kt
+++ app/src/main/java/ru/noartem/first/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.noartem.first.ui.theme.FirstTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +25,10 @@ class MainActivity : ComponentActivity() {
             FirstTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
                     Greeting(
-                        name = "World"
+                        name = "World",
+                        modifier = Modifier.padding(PaddingValues(
+                            all = 120.dp,
+                        ))
                     )
                 }
             }
