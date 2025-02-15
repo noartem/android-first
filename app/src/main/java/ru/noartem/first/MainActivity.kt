1b09e18042d8a00a316beab3e431ab71ef598155
Noskov Artem <nowasmawesome@gmail.com>
2025-02-15 16:09:59 +0500

    feat: add format price

--- app/src/main/java/ru/noartem/first/MainActivity.kt
+++ app/src/main/java/ru/noartem/first/MainActivity.kt
@@ -1,11 +1,11 @@
 package ru.noartem.first
 
-import android.annotation.SuppressLint
+import android.icu.text.NumberFormat
+import android.icu.util.ULocale
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,27 +13,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import ru.noartem.first.ui.theme.FirstTheme
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FirstTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    Greeting(
-                        name = "South Ural State University",
-                        modifier = Modifier.padding(PaddingValues(
-                            all = 120.dp,
-                        ))
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = formatPrice(1234.56, ULocale.US),
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+}
+
+fun formatPrice(price: Double, locale: ULocale = ULocale.getDefault()): String {
+    val currencyFormatter = NumberFormat.getCurrencyInstance(locale)
+    return currencyFormatter.format(price)
 }
 
 @Composable
